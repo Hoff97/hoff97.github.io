c03f42e17576b1ada177751a4dfaa559baf1c7aa
Frithjof Winkelmann <frithjof97@web.de>
2020-08-31 15:44:32 +0200

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Frithjof Winkelmann'
+SITENAME = 'haskai'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/Hoff97'),
+          ('LinkedIn', 'https://www.linkedin.com/in/frithjof-winkelmann-338a99a8/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
